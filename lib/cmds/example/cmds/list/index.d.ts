5bf29828a05027e2fd12f910e50ea9945ca3df13
Gregory Markou <16929357+GregTheGreek@users.noreply.github.com>
2021-02-28 15:46:53 -0500

    updates (#3)

--- lib/cmds/example/cmds/list/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ICliCommand } from "../../../../util";
-import { IGlobalArgs } from "../../../../options";
-import { IListArgs } from "./options";
-export declare type ReturnType = string;
-export declare const helloWorld: ICliCommand<Record<never, never>, IListArgs & IGlobalArgs, ReturnType>;
--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,4 @@
-import {initBLS} from "@chainsafe/bls";
 import {before} from "mocha";
 
 before(async function() {
-  await initBLS();
 });
